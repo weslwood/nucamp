e7facc3f83b065089bf925faf95d9f2d98f8573f
weslwood <weslwood@hotmail.com>
2021-12-24 19:45:40 -0500

    Icons, Favorites, and Comments

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
+import Home from "./HomeComponent";
 import Directory from "./DirectoryComponent";
-import { CAMPSITES } from "../shared/campsites";
 import CampsiteInfo from "./CampsiteInfoComponent";
 import Constants from "expo-constants";
+import About from "./AboutComponent";
+import Contact from "./ContactComponent";
 import { View, Platform } from "react-native";
 import { createStackNavigator } from "react-navigation-stack";
+import { createDrawerNavigator } from "react-navigation-drawer";
 import { createAppContainer } from "react-navigation";
 
 const DirectoryNavigator = createStackNavigator(
@@ -26,7 +29,70 @@ const DirectoryNavigator = createStackNavigator(
   }
 );
 
-const AppNavigator = createAppContainer(DirectoryNavigator);
+const AboutNavigator = createStackNavigator(
+  {
+    About: { screen: About },
+  },
+  {
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: "#5637DD",
+      },
+      headerTintColor: "#fff",
+      headerTitleStyle: {
+        color: "#fff",
+      },
+    },
+  }
+);
+
+const ContactNavigator = createStackNavigator(
+  {
+    Contact: { screen: Contact },
+  },
+  {
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: "#5637DD",
+      },
+      headerTintColor: "#fff",
+      headerTitleStyle: {
+        color: "#fff",
+      },
+    },
+  }
+);
+
+const HomeNavigator = createStackNavigator(
+  {
+    Home: { screen: Home },
+  },
+  {
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: "#5637DD",
+      },
+      headerTintColor: "#fff",
+      headerTitleStyle: {
+        color: "#fff",
+      },
+    },
+  }
+);
+
+const MainNavigator = createDrawerNavigator(
+  {
+    Home: { screen: HomeNavigator },
+    Directory: { screen: DirectoryNavigator },
+    About: { screen: AboutNavigator },
+    Contact: { screen: ContactNavigator },
+  },
+  {
+    drawerBackgroundColor: "#CEC8FF",
+  }
+);
+
+const AppNavigator = createAppContainer(MainNavigator);
 
 class Main extends Component {
   render() {
